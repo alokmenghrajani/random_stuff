9e21a7be1b8cbad8497a76c6a6624ea842ac7d73
Alok Menghrajani <alok@fb.com>
2011-10-17 20:00:05 -0700

    85 characters
    Finally found a way to remove the map. Also shaved a few characters by replacing the ||1 with +1 and reducing all the numbers in the map by -1.

--- perl_golf/roman_numeral.pl
+++ perl_golf/roman_numeral.pl
@@ -1,1 +1,1 @@
-%m=(M,1e3,D,5e2,C,1e2,L,50,X,10,V,5);map{$x=$p;$r+=($p=$m{$_}||1)>$x?-$x:$x}"@ARGV"=~/./g;print$r+$p
+map{$x=$p;"M999D499C99L49X9V4"=~/$_/;$r+=($p=$'+1)>$x?-$x:$x}"@ARGV"=~/./g;print$r+$p
