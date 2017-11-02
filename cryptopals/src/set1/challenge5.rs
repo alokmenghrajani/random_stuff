cb9174d116913f7ccd348d1028ff0d79b19a9000
Alok Menghrajani <alok@squareup.com>
2017-11-01 22:17:41 -0700

    set 1, challenge 6

--- cryptopals/src/set1/challenge5.rs
+++ cryptopals/src/set1/challenge5.rs
@@ -31,7 +31,7 @@ pub fn run() {
                "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
 }
 
-fn repeated_xor(input1: &Vec<u8>, input2: &Vec<u8>) -> Vec<u8> {
+pub fn repeated_xor(input1: &Vec<u8>, input2: &Vec<u8>) -> Vec<u8> {
     let mut r = Vec::new();
     for i in 0..input1.len() {
         r.push(input1[i] ^ input2[i % input2.len()]);
--- cryptopals/src/set1/challenge6.rs
+++ cryptopals/src/set1/challenge6.rs
@@ -8,16 +8,20 @@
  *
  * The challenge comes with step-by-step instructions but I decided to give it a try
  * with my own ideas.
+ *
  * A xor K xor B xor K == A xor B.
- * All the bytes have the MSB at 0. This implies the text and key are ascii.
- * We can try to xor the data with itself shift by N bytes.
+ * I xor the data with itself, shifted by N bytes. I then compare the distribution of
+ * bytes with a reference text which gets the same transformation.
  */
 
 use utils::base64::base64_decode;
+use set1::challenge3::single_byte_xor;
+use set1::challenge5::repeated_xor;
 
 use std::io::prelude::*;
 use std::fs::File;
 use std::io::BufReader;
+use std::f32;
 
 pub fn run() {
     // load lines from 6.txt
@@ -26,18 +30,88 @@ pub fn run() {
     let contents: String = lines.map(|x| x.unwrap()).collect();
     let data = base64_decode(contents.as_str());
 
-    for i in 2..40 {
-        let d = xor_with_shift(&data, i);
-        println!("{:?}", d);
-        print!("{}: ", i);
-        for b in d {
-            print!("{}", (b as i32) >> 8);
+    // load some reference text
+    let mut file = File::open("./src/set1/english.txt").expect("Unable to open english.txt");
+    let mut english = Vec::new();
+    file.read_to_end(&mut english).expect("unable to read file");
+
+    println!("{:?}", data);
+
+    // the best result is when the score is minimal
+    println!("Guessing key length");
+    let mut best = (f32::MAX, 0);
+    for i in 2..41 {
+        let d = xor_with_shift(&data[..], i);
+        let hist1 = histogram(&d[..]);
+
+        let e = xor_with_shift(&english[..], i);
+        let hist2 = histogram(&e[..]);
+
+        let score = histogram_diff(hist1, hist2);
+        if score < best.0 {
+            best = (score, i);
         }
-        println!("");
+        println!("{} {}", i, score);
     }
+    println!("Best: {:?}", best);
+    println!("");
+
+    // now break the key, one character at a time
+    let hist = histogram(&english[..]);
+
+    let j = best.1;
+    let mut key = String::new();
+    for i in 0..j {
+        // take every jth character from data
+        let mut d = Vec::with_capacity(data.len() / (j as usize));
+        let mut k = i as usize;
+        while k < data.len() {
+            d.push(data[k]);
+            k += j as usize;
+        }
+        let mut best = (f32::MAX, 257);
+        for k in 0..256 {
+            let e = single_byte_xor(&d, k as u8);
+            let f = histogram(&e[..]);
+            let score = histogram_diff(hist, f);
+            if score < best.0 {
+                best = (score, k);
+            }
+        }
+        key.push(best.1 as u8 as char);
+        println!("{} {} {}", i, best.0, best.1);
+    }
+    println!("key: {}", key);
+
+    let plaintext = repeated_xor(&data, &key.into_bytes());
+    let p: String = plaintext.into_iter().map(|x| x as char).collect();
+    println!("{}", p);
 }
 
-fn xor_with_shift(input: &Vec<u8>, shift: i32) -> Vec<u8> {
+fn histogram_diff(a: [f32; 256], b: [f32; 256]) -> f32 {
+    let mut diff = 0.0;
+    for (l, r) in a.iter().zip(b.iter()) {
+        diff += (l - r) * (l - r);
+    }
+    return diff;
+}
+
+fn histogram(input: &[u8]) -> [f32; 256] {
+    let mut histogram = [0.0; 256];
+
+    for i in input {
+        histogram[*i as usize] += 1.0;
+    }
+
+    let sum = histogram.iter().sum();
+    for x in histogram.iter_mut() {
+        *x /= sum;
+    }
+
+    return histogram;
+}
+
+fn xor_with_shift(input: &[u8], shift: i32) -> Vec<u8> {
     let mut r = Vec::with_capacity(input.len());
     for i in 0..input.len() {
         let j = (i + shift as usize) % input.len();
